e40dcc517e9da942a82ffbc102d6d78654672c62
kevinbett <bettkevin757@gmail.com>
2019-01-19 00:18:32 +0300

    feat(token_auth)  - Adds methond to encode token

--- project/server/models.py
+++ project/server/models.py
@@ -1,4 +1,5 @@
 import datetime
+import jwt
 
 from project.server import app, db, bcrypt
 
@@ -20,4 +21,22 @@ class User(db.Model):
         ).decode()
         self.registered_on = datetime.datetime.now()
         self.admin = admin
-        
+
+    def encode_auth_token(self, user_id):
+        """
+        Generates the Auth Token
+        :return: string
+        """
+        try:
+            payload = {
+                'exp': datetime.datetime.utcnow() + datatime.timedleta(days=0, seconds=5),
+                'iat': datetime.datetime.utcnow(),
+                'sub': user_id
+            }
+            return jwt.encode(
+                payload,
+                app.config.get('SECRET_KEY'),
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e
